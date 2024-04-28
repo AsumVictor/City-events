8a340a8ff5b7091964fb29e37512a0dca4e588b4
AsumVictor <iamasum369@gmail.com>
2024-04-28 09:49:40 +0000

    Refactor: Make fucntion reusable to get open

--- server/lib/getOpeningHours.js
+++ server/lib/getOpeningHours.js
@@ -1,14 +1,4 @@
 // Define the open hours for each day
-const openHours = {
-  monday: ["11:30", "12:00"],
-  tuesday: ["11:30", "12:00"],
-  wednesday: ["11:30", "12:00"],
-  thursday: ["11:30", "12:00"],
-  friday: ["11:30", "12:00"],
-  saturday: ["11:30", "12:00"],
-  sunday: ["08:30", "12:00"],
-};
-
 const days = {
   0: "sunday",
   1: "monday",
@@ -22,8 +12,9 @@ const days = {
 function isOpen(openHours) {
   const date = new Date();
   const currentDay = days[date.getDay()];
-  const openTime = addTime(openHours[currentDay][0]);
-  const closeTime = addTime(openHours[currentDay][1]);
+
+  const openTime = addTime(openHours.get(currentDay)[0]);
+  const closeTime = addTime(openHours.get(currentDay)[1]);
 
   const hours = date.getHours().toString().padStart(2, "0");
   const minutes = date.getMinutes().toString().padStart(2, "0");
