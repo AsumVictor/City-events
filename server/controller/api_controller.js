10771a6a9bce08a04c513233553116e072b95e8c
AsumVictor <iamasum369@gmail.com>
2024-04-28 07:52:15 +0000

    Added (core): Route Logic to add new place to database

--- server/controller/api_controller.js
+++ server/controller/api_controller.js
@@ -1,27 +1,44 @@
 const express = require("express");
-const Router = express.Router()
+const Router = express.Router();
+const Model = require("../model/api-model");
+const CatchAsynError = require("../middleware/catchAsyncError");
+const ErrorHandler = require("../lib/ErrorHandler");
 
-Router.post("/", (req, res)=>{
-    res.json({message: 'Add new Place'})
-})
+Router.post(
+  "/",
+  CatchAsynError(async (req, res, next) => {
+    try {
+      await Model.create(req.body);
+      res.json({ message: "Add new Place" });
+    } catch (error) {
+      return next(new ErrorHandler(error, 400));
+    }
+  })
+);
 
-Router.get("/", (req, res)=>{
-    res.json({message: 'Get all places'})
-})
+Router.get(
+  "/",
+  CatchAsynError(async (req, res, next) => {
+    res.json({ message: "Get all places" });
+  })
+);
 
-Router.get("/:id", (req, res)=>{
-    res.json({message: 'Get specfic place'})
-})
+Router.get(
+  "/:id",
+  CatchAsynError(async (req, res, next) => {
+    res.json({ message: "Get specfic place" });
+  })
+);
 
-Router.patch("/", (req, res)=>{
-    res.json({message: 'Update a specfic place'})
-})
+Router.patch(
+  "/",
+  CatchAsynError(async (req, res, next) => {
+    res.json({ message: "Update a specfic place" });
+  })
+);
 
-Router.delete("/", (req, res)=>{
-    res.json({message: 'delete a specfic place'})
-})
+Router.delete("/", async (req, res, next) => {
+  res.json({ message: "delete a specfic place" });
+});
 
-
-
-
-module.exports = Router
+module.exports = Router;
